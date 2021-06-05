ba195fe85cffd4e211f4101c8228db4c6ec76fa0
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2021-06-04 22:17:43 -0400

    Completed code.

--- src/main/java/ex12/App.java
+++ src/main/java/ex12/App.java
@@ -4,9 +4,26 @@
  */
 package ex12;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args){
 
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the principal: ");
+        double principal = in.nextDouble();
+
+        System.out.print("Enter the rate of interest: ");
+        double rate = in.nextDouble();
+
+        System.out.print("Enter the number of years: ");
+        int years = in.nextInt();
+
+        double result = principal * (1 + ((rate/100) * years));
+        result = Math.ceil(result*100)/100;
+
+        System.out.println("After " + years + " years at " + rate + "%, the investment will be worth $" + result + ".");
 
     }
 }
