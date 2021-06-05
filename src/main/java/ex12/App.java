93671510f98b57334d05db8eb05cddc0d53e7f63
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2021-06-04 21:55:08 -0400

    Initial commit

---
+++ src/main/java/ex12/App.java
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Evan Raiford
+ */
+package ex12;
+
+public class App {
+    public static void main(String[] args){
+
+
+    }
+}
